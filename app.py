89675ca5a349ae43d7ba278fa07c4d8a6ca2f559
Michelle Davis <67066019+MHargita@users.noreply.github.com>
2021-01-21 18:48:12 -0500

    added app.py file for flask

---
+++ app.py
@@ -0,0 +1,35 @@
+# 1. import Dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
+from flask import Flask, jsonify
+import numpy as np
+
+engine = create_engine()
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to the Beer Dashboard!"
+
+
+# 4. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to the 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
