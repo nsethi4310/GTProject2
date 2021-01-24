b88d450754f7e6b6954b2efb98bf6d3b46f5f531
jjying89 <70499413+jjying89@users.noreply.github.com>
2021-01-23 23:02:57 -0500

    update
    app.py, static, templates

---
+++ app.py
@@ -0,0 +1,90 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, render_template, jsonify
+import pandas as pd
+
+####################
+# assign a variable that contains a string of your credentials
+####################
+credentials = "sqlite:///Resources/beer_test.sqlite"
+
+####################
+# read in your SQL query results using pandas
+####################
+Breweries_df = pd.read_sql("SELECT * FROM Breweries", con = credentials)
+Breweries = Breweries_df.to_json(orient = "index")
+
+ba_beerstyles_df = pd.read_sql("SELECT * FROM ba_beerstyles", con = credentials)
+ba_beerstyles = ba_beerstyles_df.to_json()
+
+us_state_data_df = pd.read_sql("SELECT * FROM us_state_data", con = credentials)
+us_state_data = us_state_data_df.to_json()
+
+usa_breweries_df = pd.read_sql("SELECT * FROM usa_breweries", con = credentials)
+usa_breweries = usa_breweries_df.to_json()
+
+####################
+# database setup
+####################
+# engine = create_engine("sqlite:///Resources/beer_test.sqlite")
+
+# reflect an existing database into a new model
+# Base = automap_base()
+
+# reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# check tables
+# Base.metadata.tables
+
+# get table names
+# Base.classes.keys()
+
+# save reference to the table (from hw, leaving it here as reference)
+# does not work due to lack of primary keys in database 
+# M = Base.classes.measurement
+# S = Base.classes.station
+
+# Create our session (link) from Python to the DB
+# session = Session(engine)
+
+####################
+# flask setup
+####################
+app = Flask(__name__)
+
+####################
+# flask routes
+####################
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/map")
+def map():
+    return render_template('map.html')    
+
+@app.route("/map_data")
+def map_data():
+    return Breweries
+
+@app.route("/ba_beerstyles")
+def ba_beerstyles():
+    return ba_beerstyles
+
+@app.route("/us_state_data")
+def us_state_data():
+    return us_state_data
+
+@app.route("/usa_breweries")
+def usa_breweries():
+    return usa_breweries
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
